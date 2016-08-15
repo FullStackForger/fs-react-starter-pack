4dbc3a4e9203b295bec591653f21702fbcbe676e
FullStackForger <fullstackforger@gmail.com>
2016-08-15 14:56:37 +0100

    store->config/store

--- src/app.js
+++ src/app.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import Layout from './components/layout';
 import {Provider} from 'react-redux'
-import store from './store'
+import store from './config/store'
 
 ReactDOM.render(
 	<Provider store={store}>
