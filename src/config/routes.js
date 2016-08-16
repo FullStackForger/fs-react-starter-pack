88250cc583e8bc923becf248795f0acde90d9adc
FullStackForger <fullstackforger@gmail.com>
2016-08-16 16:39:27 +0100

    auth routing refactored

--- src/config/routes.js
+++ src/config/routes.js
@@ -7,6 +7,12 @@ import Profile from '../components/page-profile/component'
 import Logout from '../components/page-logout/component'
 import Signup from '../components/page-signup/component'
 
+import auth from '../utils/auth'
+
+const loginCheck = (nextState, replace, authState, redirectPath) => {
+	if (auth.isLoggedIn() !== authState) replace(redirectPath)
+}
+
 const routes = {
   path: '/',
   component: Layout,
@@ -14,11 +20,11 @@ const routes = {
   childRoutes: [
 		{ path: '/home', component: Home },
 		{ path: '/info', component: Info },
-		{ path: '/login', component: Login },
-		{ path: '/signup', component: Signup },
-		{ path: '/logout', component: Logout },
-		{ path: '/account', component: Account },
-		{ path: '/profile', component: Profile }
+		{ path: '/login', component: Login, onEnter: (nextState, replace) => loginCheck(nextState, replace, false, '/') },
+		{ path: '/signup', component: Signup, onEnter: (nextState, replace) => loginCheck(nextState, replace, false, '/')  },
+		{ path: '/logout', component: Logout, onEnter: (nextState, replace) => loginCheck(nextState, replace, true, '/')  },
+		{ path: '/account', component: Account, onEnter: (nextState, replace) => loginCheck(nextState, replace, true, '/login')  },
+		{ path: '/profile', component: Profile, onEnter: (nextState, replace) => loginCheck(nextState, replace, true, '/login')  }
 	]
 }
 
