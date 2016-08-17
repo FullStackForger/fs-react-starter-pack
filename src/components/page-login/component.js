4c9bc4c28a8446a8456091e37c2b265fc8f24ed1
FullStackForger <fullstackforger@gmail.com>
2016-08-18 00:47:52 +0100

    login and siignup templates

--- src/components/page-login/component.js
+++ src/components/page-login/component.js
@@ -1,4 +1,37 @@
 import React from 'react'
-export default ()=>(
-	<h3>Login</h3>
+
+import { Grid, Col, Panel } from 'react-bootstrap'
+import { Form, FormGroup, FormControl } from 'react-bootstrap'
+import { Checkbox, Button, ControlLabel } from 'react-bootstrap'
+
+export default () => (
+	<Grid>
+		<Col md={6} mdPush={3}>
+			<Panel>
+				<Form horizontal>
+					<FormGroup controlId="formHorizontalEmail">
+						<Col componentClass={ControlLabel} sm={2}>
+							Email
+						</Col>
+						<Col sm={10}>
+							<FormControl type="email" placeholder="Email" />
+						</Col>
+					</FormGroup>
+
+					<FormGroup controlId="formHorizontalPassword">
+						<Col componentClass={ControlLabel} sm={2}>
+							Password
+						</Col>
+						<Col sm={10}>
+							<FormControl type="password" placeholder="Password" />
+						</Col>
+					</FormGroup>
+
+					<Button bsStyle="primary" bsSize="large" block>
+						Log in
+					</Button>
+				</Form>
+			</Panel>
+		</Col>
+	</Grid>
 )
--- src/components/page-signup/component.js
+++ src/components/page-signup/component.js
@@ -1,4 +1,57 @@
 import React from 'react'
-export default ()=>(
-	<h3>Signup</h3>
+
+import { Grid, Col, Panel } from 'react-bootstrap'
+import { Form, FormGroup, FormControl } from 'react-bootstrap'
+import { FieldGroup } from 'react-bootstrap'
+import { Checkbox, Button, ControlLabel } from 'react-bootstrap'
+
+export default () => (
+	<Grid>
+		<Col md={6} mdPush={3}>
+			<Panel>
+				<Form horizontal>
+
+					<FormGroup controlId="signupUserName">
+						<Col componentClass={ControlLabel} sm={2}>
+							Username
+						</Col>
+						<Col sm={10}>
+							<FormControl type="email" placeholder="Email" />
+						</Col>
+					</FormGroup>
+
+					<FormGroup controlId="signupEmail">
+						<Col componentClass={ControlLabel} sm={2}>
+							Email
+						</Col>
+						<Col sm={10}>
+							<FormControl type="email" placeholder="Email" />
+						</Col>
+					</FormGroup>
+
+					<FormGroup controlId="signupPassword">
+						<Col componentClass={ControlLabel} sm={2}>
+							Password
+						</Col>
+						<Col sm={10}>
+							<FormControl type="password" placeholder="Password" />
+						</Col>
+					</FormGroup>
+
+					<FormGroup controlId="signupPasswordConfirmation">
+						<Col componentClass={ControlLabel} sm={2}>
+							Password
+						</Col>
+						<Col sm={10}>
+							<FormControl type="password" placeholder="Confirm Password" />
+						</Col>
+					</FormGroup>
+
+					<Button bsStyle="primary" bsSize="large" block>
+						Sign up
+					</Button>
+				</Form>
+			</Panel>
+		</Col>
+	</Grid>
 )
