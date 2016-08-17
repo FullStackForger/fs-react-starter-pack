3b4368a12223fc30f86e7993f28d578c3aba4f20
FullStackForger <fullstackforger@gmail.com>
2016-08-17 17:20:20 +0100

    react-router-dedux setup

--- src/app.js
+++ src/app.js
@@ -2,14 +2,17 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux'
 import { Router, browserHistory, hashHistory } from 'react-router'
+import { syncHistoryWithStore } from 'react-router-redux'
 
 import Layout from './components/layout';
 import store from './config/store'
 import routes from './config/routes'
 
+const history = syncHistoryWithStore(hashHistory, store)
+
 ReactDOM.render(
 	<Provider store={store}>
-		<Router history={hashHistory} routes={routes} />
+		<Router history={history} routes={routes} />
 	</Provider>,
 	document.getElementById('app')
 );
--- src/config/store.js
+++ src/config/store.js
@@ -1,8 +1,11 @@
-import {createStore, combineReducers} from 'redux'
+import { createStore, combineReducers } from 'redux'
+import { routerReducer } from 'react-router-redux'
 
 const reducers = combineReducers({
-	dummy: (state = {}, action) => (state)
+	dummy: (state = {}, action) => (state),
+	routing: routerReducer
 })
+
 const store = createStore(reducers)
 
 export default store
