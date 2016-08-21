e6fa21cd4ac79f6b0fbb750b3097391bd6696a74
FullStackForger <fullstackforger@gmail.com>
2016-08-21 15:29:52 +0100

    navbar is now a class

--- src/components/page-navbar/component.js
+++ src/components/page-navbar/component.js
@@ -3,6 +3,7 @@ import { Nav, Navbar, NavItem, NavDropdown, MenuItem } from 'react-bootstrap';
 import { FormGroup, FormControl, Button } from 'react-bootstrap'
 import { LinkContainer } from 'react-router-bootstrap'
 
+
 //todo: move it to container perhaps
 import auth from '../../utils/auth'
 import history from '../../config/history'
@@ -11,46 +12,57 @@ const onLogoutClick = () => {
 	history.push('/logout')
 }
 
-const PageNavBar = () => (
-  <Navbar>
-    <Navbar.Header>
-      <Navbar.Brand>
-        <a href="#">Brand</a>
-      </Navbar.Brand>
-      <Navbar.Toggle />
-    </Navbar.Header>
-    <Navbar.Collapse>
-      <Nav>
-				<LinkContainer to='/home'>
-					<NavItem eventKey={1} href="#">Home</NavItem>
-				</LinkContainer>
-				<LinkContainer to='/info'>
-					<NavItem eventKey={2} href="#">Info</NavItem>
-				</LinkContainer>
-      </Nav>
+class PageNavBar extends React.Component {
 
-			<Nav pullRight>
-				<LinkContainer to='/login'>
-					<NavItem eventKey={5} href="#">Login</NavItem>
-				</LinkContainer>
-				<LinkContainer to='/signup'>
-					<NavItem eventKey={6} href="#">Signup</NavItem>
-				</LinkContainer>
-			</Nav>
+	constructor(props) {
+		super(props)
+		this.onLogoutClick = onLogoutClick
+	}
+	render() {
+		let authenticated = true
 
-			<Nav pullRight>
-				<NavDropdown eventKey={7} title="Dropdown" id="basic-nav-dropdown">
-					<LinkContainer to='/account'>
-						<MenuItem eventKey={7.1}>Account</MenuItem>
-					</LinkContainer>
-					<MenuItem divider />
-					<MenuItem eventKey={7.2} onClick={onLogoutClick}>Logout</MenuItem>
-				</NavDropdown>
-			</Nav>
-
-
-    </Navbar.Collapse>
-  </Navbar>
-);
+		return (
+			<Navbar>
+				<Navbar.Header>
+					<Navbar.Brand>
+						<a href="#">Brand</a>
+					</Navbar.Brand>
+					<Navbar.Toggle />
+				</Navbar.Header>
+				<Navbar.Collapse>
+					<Nav>
+						<LinkContainer to='/home'>
+							<NavItem eventKey={1} href="#">Home</NavItem>
+						</LinkContainer>
+						<LinkContainer to='/info'>
+							<NavItem eventKey={2} href="#">Info</NavItem>
+						</LinkContainer>
+					</Nav>
+					{(() => { if (authenticated) { return (
+						<Nav pullRight>
+							<LinkContainer to='/login'>
+								<NavItem eventKey={5} href="#">Login</NavItem>
+							</LinkContainer>
+							<LinkContainer to='/signup'>
+								<NavItem eventKey={6} href="#">Signup</NavItem>
+							</LinkContainer>
+						</Nav>
+					)} else { return (
+						<Nav pullRight>
+							<NavDropdown eventKey={7} title="Dropdown" id="basic-nav-dropdown">
+								<LinkContainer to='/account'>
+									<MenuItem eventKey={7.1}>Account</MenuItem>
+								</LinkContainer>
+								<MenuItem divider />
+								<MenuItem eventKey={7.2} onClick={this.onLogoutClick}>Logout</MenuItem>
+							</NavDropdown>
+						</Nav>
+					)}
+				})()}
+				</Navbar.Collapse>
+			</Navbar>
+		)
+	}
+}
 
 export default PageNavBar
