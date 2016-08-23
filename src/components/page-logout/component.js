d720d7076c6cb536893480a2e44cd26d9a256e7e
FullStackForger <fullstackforger@gmail.com>
2016-08-23 03:41:47 +0100

    timer fix

--- src/components/page-logout/component.js
+++ src/components/page-logout/component.js
@@ -6,22 +6,33 @@ class LogoutPage extends React.Component {
 
 	constructor(props) {
 		super(props)
+		this.timer = null
 		this.state = {
 			timeout: 3
 		}
 	}
 
 	componentDidMount() {
-		let timer = setInterval(() => {
+		this.timer = setInterval(() => {
 			let timeout = this.state.timeout - 1
 			if (timeout < 0) {
-				clearTimeout(timer)
-				return
+				return this.clearTimer()
 			}
 			this.setState({
 				timeout: timeout
 			})
 		}, 1000)
+	}
+
+	componentWillUnmount() {
+		if (this.timer != null) {
+			this.clearTimer()
+		}
+	}
+
+	clearTimer() {
+		clearInterval(this.timer)
+		this.timer = null
 	}
 
 	render() {
