d133f247d1918466ad0177865c2b5420f3f99905
FullStackForger <fullstackforger@gmail.com>
2016-08-23 03:28:21 +0100

    logout page

--- src/components/page-logout/component.js
+++ src/components/page-logout/component.js
@@ -1,4 +1,44 @@
-import React from 'react'
-export default ()=>(
-	<h3>Logout</h3>
-)
+import React, { PropTypes } from 'react'
+import { Grid, Col, Panel, HelpBlock } from 'react-bootstrap'
+import { Link } from 'react-router'
+
+class LogoutPage extends React.Component {
+
+	constructor(props) {
+		super(props)
+		this.state = {
+			timeout: 3
+		}
+	}
+
+	componentDidMount() {
+		let timer = setInterval(() => {
+			let timeout = this.state.timeout - 1
+			if (timeout < 0) {
+				clearTimeout(timer)
+				return
+			}
+			this.setState({
+				timeout: timeout
+			})
+		}, 1000)
+	}
+
+	render() {
+		return (
+			<Grid>
+				<Col md={6} mdPush={3}>
+					<Panel>
+						<HelpBlock style={{textAlign: 'center'}}>
+							<p><strong>You have been logged out.</strong></p>
+							<p>Redirecting to home page... in {this.state.timeout}s</p>
+						</HelpBlock>
+					</Panel>
+
+				</Col>
+			</Grid>
+		)
+	}
+}
+
+export default LogoutPage
