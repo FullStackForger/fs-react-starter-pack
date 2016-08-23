74269f32ba688dafb8e6e7b255000fa0f5e87340
FullStackForger <fullstackforger@gmail.com>
2016-08-23 02:52:06 +0100

    cleanup

--- src/components/page-navbar/container.js
+++ src/components/page-navbar/container.js
@@ -2,12 +2,10 @@ import {connect} from 'react-redux'
 import PageNavbar from './component'
 import auth from '../auth/auth'
 
-const mapStateToProps = (state) => {
-	return {
+const mapStateToProps = (state) => ({
 		// updateds navbar state based on auth state
 		authenticated: state.auth.authenticated
-	}
-}
+})
 
 export default connect(
 	mapStateToProps
