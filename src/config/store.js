e5af6c288644bdcd6a49c9aacdc8f9fbc0a7cf8a
FullStackForger <fullstackforger@gmail.com>
2016-08-25 19:14:53 +0100

    react devtools

--- src/config/store.js
+++ src/config/store.js
@@ -1,8 +1,15 @@
-import { createStore } from 'redux'
+import { createStore, compose } from 'redux'
 
 import reducers from './reducers'
 import middleware from './middleware'
+import DevTools from './devtools'
 
-const store = createStore(reducers, middleware)
+//const store = createStore(reducers, middleware)
+
+const initialState = {}
+const store = createStore(reducers, initialState, compose(
+	middleware,
+	window.devToolsExtension ? window.devToolsExtension() : DevTools.instrument()
+));
 
 export default store
