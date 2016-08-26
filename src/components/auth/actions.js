ef7daafc63205158e652ee27e4e9a50fe495de41
FullStackForger <fullstackforger@gmail.com>
2016-08-26 19:57:53 +0100

    const renamed

--- src/components/auth/actions.js
+++ src/components/auth/actions.js
@@ -1,11 +1,11 @@
-export const AUTH_LOGIN = 'AUTH_LOGIN'
-export const AUTH_LOGIN_SUCCESS = 'AUTH_LOGIN_SUCCESS'
-export const AUTH_LOGIN_FAILED = 'AUTH_LOGIN_FAILED'
-export const AUTH_LOGOUT = 'AUTH_LOGOUT'
-export const AUTH_LOGOUT_SUCCESS = 'AUTH_LOGOUT_SUCCESS'
-export const AUTH_SIGNUP = 'AUTH_SIGNIN'
-export const AUTH_SIGNUP_SUCCESS = 'AUTH_SIGNUP_SUCCESS'
-export const AUTH_SIGNUP_FAILED = 'AUTH_SIGNUP_FAILED'
+export const AUTH_LOGIN 					= '@AUTH/LOGIN'
+export const AUTH_LOGIN_SUCCESS 	= '@AUTH/LOGIN_SUCCESS'
+export const AUTH_LOGIN_FAILED 		= '@AUTH/LOGIN_FAILED'
+export const AUTH_LOGOUT 					= '@AUTH/LOGOUT'
+export const AUTH_LOGOUT_SUCCESS 	= '@AUTH/LOGOUT_SUCCESS'
+export const AUTH_SIGNUP 					= '@AUTH/SIGNIN'
+export const AUTH_SIGNUP_SUCCESS 	= '@AUTH/SIGNUP_SUCCESS'
+export const AUTH_SIGNUP_FAILED 	= '@AUTH/SIGNUP_FAILED'
 
 export const login = (userData) => ({
 	type: AUTH_LOGIN,
