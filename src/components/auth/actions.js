34fb149833d96d19fa10e719a1d25ed5a8aeadf6
FullStackForger <fullstackforger@gmail.com>
2016-08-30 05:21:56 +0100

    typo

--- src/components/auth/actions.js
+++ src/components/auth/actions.js
@@ -3,7 +3,7 @@ export const AUTH_LOGIN_SUCCESS 	= '@@auth/LOGIN_SUCCESS'
 export const AUTH_LOGIN_FAILED 		= '@@auth/LOGIN_FAILED'
 export const AUTH_LOGOUT 					= '@@auth/LOGOUT'
 export const AUTH_LOGOUT_SUCCESS 	= '@@auth/LOGOUT_SUCCESS'
-export const AUTH_SIGNUP 					= '@@auth/SIGNIN'
+export const AUTH_SIGNUP 					= '@@auth/SIGNUP'
 export const AUTH_SIGNUP_SUCCESS 	= '@@auth/SIGNUP_SUCCESS'
 export const AUTH_SIGNUP_FAILED 	= '@@auth/SIGNUP_FAILED'
 
