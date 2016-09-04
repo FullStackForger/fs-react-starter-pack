398786dac4e2a5126ec1860ccde37c79f0b1087f
FullStackForger <fullstackforger@gmail.com>
2016-09-04 18:21:26 +0100

    utils externalized

---
+++ src/components/auth/internals/utils.js
@@ -0,0 +1,13 @@
+export const checkResponseStatus = (response) => {
+  if (response.status >= 200 && response.status < 300) {
+    return response
+  } else {
+    var error = new Error(response.statusText)
+    error.response = response
+    throw error
+  }
+}
+
+export const parseResponseToJSON = (response) => {
+  return response.json()
+}
