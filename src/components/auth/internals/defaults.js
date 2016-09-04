dcc3795fa37c4753ecd1864132f55180459320f0
FullStackForger <fullstackforger@gmail.com>
2016-09-04 18:21:49 +0100

    auth defaults

---
+++ src/components/auth/internals/defaults.js
@@ -0,0 +1,14 @@
+export const configOpts = {
+	tokenName: 'if-token',
+	baseUrl: '/',
+	loginUrl: 'auth/login',
+	signupUrl: 'auth/signup'
+}
+
+export const fetchOpts ={
+	method: 'POST',
+	headers: {
+		'Accept': 'application/json',
+		'Content-Type': 'application/json'
+	}
+}
