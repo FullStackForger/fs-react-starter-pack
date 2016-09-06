e1f5c3997a24ea931d8e2a171781f03fb7c7b6b1
FullStackForger <fullstackforger@gmail.com>
2016-09-06 22:31:39 +0100

    auth-redux wrapped local

---
+++ src/components/auth-redux/wrapped-local.js
@@ -0,0 +1,41 @@
+import { auth, dispatch } from './internals'
+import * as AUTH from './constants'
+
+export const login = (userData) => {
+	dispatch(AUTH.LOGIN, userData)
+	return auth
+		.login(userData)
+		.then((token)=> {
+			dispatch(AUTH.LOGIN_SUCCESS, { token })
+		})
+		.catch((error) => {
+			console.error(error.trace)
+			dispatch(AUTH.LOGIN_FAILED, null, error)
+		})	
+}
+
+export const logout = () => {
+	dispatch(AUTH.LOGOUT)
+	return auth
+		.logout()
+		.then((token)=> {
+			dispatch(AUTH.LOGOUT_SUCCESS, { })
+		})
+		.catch((error) => {
+			console.error(error.trace)
+			dispatch(AUTH.LOGIN_FAILED, null, error)
+		})	
+}
+
+export const signup = (userData) => {
+	dispatch(AUTH.LOGIN, userData)
+	return auth
+		.login(userData)
+		.then((token)=> {
+			dispatch(AUTH.LOGIN_SUCCESS, { token })
+		})
+		.catch((error) => {
+			console.error(error.trace)
+			dispatch(AUTH.LOGIN_FAILED, null, error)
+		})	
+}
