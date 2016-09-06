8df373e896b2413b4b6c24ea1e3d9a9c318e2aa7
FullStackForger <fullstackforger@gmail.com>
2016-09-06 22:31:19 +0100

    auth-redux reducer

---
+++ src/components/auth-redux/reducer.js
@@ -0,0 +1,58 @@
+import { defaultState } from './defaults'
+import { auth } from './internals'
+import * as ACTION from './constants'
+
+const authReducer = (state = defaultState, action) => {
+	let newState = Object.assign({}, state, {
+		authenticated: auth.isAuthenticated(),
+		token: auth.getToken(),
+		updated: true
+	})
+
+	switch(action.type) {
+
+		// LOGIN && SIGNUP
+
+		case ACTION.LOGIN:
+		case ACTION.SIGNUP:
+			return Object.assign(newState, {
+				authenticating: true,
+				authenticated: false,
+				payload: action.payload
+			})
+		case ACTION.LOGIN_SUCCESS:
+		case ACTION.SIGNIN_SUCCESS:
+			return Object.assign(newState, {
+				authenticated: true,
+				authenticating: false,
+				payload: action.payload
+			})
+		case ACTION.LOGIN_FAILED:
+		case ACTION.SIGNUP_FAILED:
+			return Object.assign(newState, {
+				authenticated: false,
+				authenticating: false,
+				error: action.payload.error
+			})
+
+		// LOGOUT
+
+		case ACTION.LOGOUT:
+		return Object.assign(newState, {
+			authenticating: true
+		})
+		case ACTION.LOGOUT_SUCCESS:
+			return Object.assign(newState, {
+				authenticated: false,
+				authenticating: false
+			})
+		case ACTION.LOGOUT_FAILED:
+			return Object.assign(newState, {
+				authenticated: false,
+				authenticating: false,
+				error: action.payload.error
+			})
+	}
+
+	return Object.assign(newState, { updated: false })
+}
