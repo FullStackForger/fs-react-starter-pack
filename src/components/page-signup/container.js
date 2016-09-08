19aad0f60086443add8f1f09eff2b71890f4f92f
FullStackForger <fullstackforger@gmail.com>
2016-09-08 18:09:38 +0100

    new signup functionality

--- src/components/page-signup/container.js
+++ src/components/page-signup/container.js
@@ -1,20 +1,16 @@
 import { connect } from 'react-redux'
+import { withRouter } from 'react-router'
 
-import auth from '../auth'
+import auth from '../auth-redux'
 
 import PageSignup from './component'
 import history from '../../config/history'
 
 const mapDispatchToProps = (dispatch, ownProps) => ({
-	handleSignup (userData) {
-		let subscriberId = auth.subscribe((authStore) => {
-			if (authStore.authenticated) {
-				auth.unsubscribe(subscriberId)
-				history.push('/')
-			}
-		})
-		dispatch(auth.actions.signup(userData))
-	}
+	handleSignup: loginData => auth
+		.signup(loginData)
+		.then(() => history.push('/'))
+		.catch(err => console.error(err)) 					
 })
 
 export default connect(false, mapDispatchToProps)(PageSignup)
