34d5e51aa3656646a2d9ac172342a67ebe9a5a67
FullStackForger <fullstackforger@gmail.com>
2016-09-13 16:35:33 +0100

    token parsing

--- src/components/auth/shared.js
+++ src/components/auth/shared.js
@@ -22,4 +22,33 @@ export const getAuthHeader = () => {
     return {}    
 }
 
-export const isAuthenticated = () => (!!getToken())
+export const isAuthenticated = () => {
+    const token = getToken()
+
+    if (!token) {
+        return false
+    }
+    let claims = token.split('.')
+    if (claims.length != 3) {
+        return false
+    }
+
+    try {        
+        let public64 = claims[1]
+        let publicClaims = JSON.parse(atob(claims[1]))
+        
+        if (!publicClaims.exp) {
+            return true
+        }
+
+        let isExpTimestamp = typeof publicClaims.exp === 'number'
+        if (!isExpTimestamp) {
+            return false
+        }
+        
+        return Math.round(new Date().getTime() / 1000) < exp
+        
+    } catch (e) {
+        return false
+    }
+}
