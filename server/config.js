0e993ae755b8734c716c895a6a1d32f08b09c69c
FullStackForger <fullstackforger@gmail.com>
2016-10-14 07:33:36 +0100

    default db changed to: fs-react-starter-pack

--- server/config.js
+++ server/config.js
@@ -1,5 +1,5 @@
 module.exports = {
-	MONGO_URI: process.env.MONGO_URI || 'mongodb://localhost/fs-react-bootstrap-webpack',
+	MONGO_URI: process.env.MONGO_URI || 'mongodb://localhost/fs-react-starter-pack',
 	TOKEN_SECRET: process.env.TOKEN_SECRET || 't0k3n_s3cr3t',
 	FACEBOOK_SECRET: process.env.FACEBOOK_SECRET,
 	GOOGLE_SECRET: process.env.GOOGLE_SECRET
