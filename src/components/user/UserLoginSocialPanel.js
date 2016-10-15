f9d7df5960fd20b80138a0cdeea5397dba9c6f08
FullStackForger <fullstackforger@gmail.com>
2016-10-15 14:24:54 +0100

    component method replaced with comp. class

--- src/components/user/UserLoginSocialPanel.js
+++ src/components/user/UserLoginSocialPanel.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, {Component, PropTypes} from 'react'
 import { Col, Panel } from 'react-bootstrap'
 import { Facebook, Google } from 'react-jwt-auth-redux'
 
@@ -14,26 +14,48 @@ export const css = {
 	}
 }
 
-const UserLoginSocialPanel = ({
-	title,
-	onSignInSuccess
-}) => (
-	<Panel><Col xs={10} xsPush={1} sm={12} smPush={0}>
-		{(() => { return title ? <div style={css.panelTitle}>{title}</div> : null})()}
-		<div className="form-group">
-			<Facebook
-				className="btn btn-md btn-block"
-				clientId="310178806023492"
-				onSignInSuccess={onSignInSuccess}
-			/>
-			<Google
-				className="btn btn-md btn-block"
-				clientId="389760969675-u3h2dgm1v3lqd22u8aloimkgd10i0rvf.apps.googleusercontent.com"
-				onSignInSuccess={onSignInSuccess}
-			/>
-		</div>
-	</Col></Panel>
-)
+const propTypes = {
+	title: PropTypes.string,
+	onSignInSuccess: PropTypes.func
+}
+
+const defaultProps = {
+	title: null,
+	onSignInSuccess: () => {}
+}
+
+export default class UserLoginSocialPanel extends Component
+{
+
+	constructor(props){
+		super(props)
+	}
+
+	render () {
+		const title = this.props.title
+		const onSignInSuccess = this.props.onSignInSuccess
+
+		return (
+			<Panel><Col xs={10} xsPush={1} sm={12} smPush={0}>
+				{(() => {
+					return title ? <div style={css.panelTitle}>{title}</div> : null
+				})()}
+				<div className="form-group">
+					<Facebook
+						className="btn btn-md btn-block"
+						clientId="310178806023492"
+						onSignInSuccess={onSignInSuccess}
+					/>
+					<Google
+						className="btn btn-md btn-block"
+						clientId="389760969675-u3h2dgm1v3lqd22u8aloimkgd10i0rvf.apps.googleusercontent.com"
+						onSignInSuccess={onSignInSuccess}
+					/>
+				</div>
+			</Col></Panel>
+		)
+	}
+}
 
 UserLoginSocialPanel.css = css
-export default UserLoginSocialPanel
+UserLoginSocialPanel.propTypes = propTypes
