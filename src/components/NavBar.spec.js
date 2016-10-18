de6915aafc8489289ca63e5677b0bba8bce109bf
FullStackForger <fullstackforger@gmail.com>
2016-10-19 00:50:41 +0100

    NavBar initial spec

---
+++ src/components/NavBar.spec.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import NavBar from './Layout'
+import chai from 'chai'
+import { shallow } from 'enzyme'
+
+const expect = chai.expect
+
+
+describe('NavBar', () => {
+
+	it('should render', () => {
+		throw new Error("missing")
+	})
+
+	it('should hace basic links', () => {
+		throw new Error("missing")
+	})
+
+	it('should display guest links', () => {
+		throw new Error('missing')
+	})
+
+	it('should display restricted links', () => {
+		throw new Error('missing')
+	})
+
+})
